9033f84ccd41d5ef2e9752356a094fc25e11944c
Abui <adjiem31@gmail.com>
2022-10-17 10:06:28 +0700

    fix: api

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 function apiInstance() {
   return axios.create({
-    baseURL: 'http://www.omdbapi.com',
+    baseURL: 'https://www.omdbapi.com',
     headers: {
       'Content-Type': 'application/json',
     },
